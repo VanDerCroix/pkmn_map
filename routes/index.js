4efdaf122b19fa78483f2938ced37f6a3dfbfe55
vandercroix <pebcc12@gmail.com>
2018-02-05 04:25:37 -0500

    dynamic map

--- routes/index.js
+++ routes/index.js
@@ -16,19 +16,31 @@ var router  = express.Router();
 router.get('/', function(req, res) {
   models.User.findAll().then(function(users) {
     res.render('index', {
-      title: 'Sequelize: Express Example',
+      title: 'Lima Map',
       users: users
     });
   });
 });
 
 router.get('/map', function(req, res) {
-  models.User.findAll().then(function(users) {
+  // models.User.findAll().then(function(users) {
     res.render('map', {
-      title: 'Sequelize: Express Example',
-      users: users
+      title: 'Map, static loading'
     });
-  });
+  // });
+});
+
+router.get('/map2', function(req, res) {
+  // models.User.findAll().then(function(users) {
+    var gyms_file = require('../public/gyms.json');
+    var parks_file = require('../public/parks.json');
+    // var gyms_file = { type: "holi", arr: "g" };
+    res.render('map2', {
+      title: 'Map, dynamic loading',
+      gyms_file: JSON.stringify(gyms_file),
+      parks_file: JSON.stringify(parks_file)
+    });
+  // });
 });
 
 module.exports = router;
